ab8f63d195e49b63f21d0e2fa81cd1c4b84d5256
Maria Rodriguez Hertz <marurh20@gmail.com>
2025-08-20 22:58:42 -0400

    Handle icons across platforms

---
+++ components/Icon.tsx
@@ -0,0 +1,24 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
+import { ComponentProps } from 'react';
+import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
+
+type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof MaterialIcons>['name']>;
+type IconSymbolName = keyof typeof MAPPING;
+
+const MAPPING = {
+    'square.and.pencil': 'edit',
+    'line.3.horizontal': 'drag-handle',
+    'checkmark': 'check',
+} as IconMapping;
+
+
+export function Icon({ name, size = 24, color, style}: {
+    name: IconSymbolName,
+    size?: number,
+    color: string | OpaqueColorValue,
+    style?: StyleProp<TextStyle>,
+    weight?: SymbolWeight
+}) {
+    return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />
+}
