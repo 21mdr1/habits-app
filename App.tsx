f1b29f59f5dbfaa1405616c0cd9c7606dfc1a534
Maria Rodriguez Hertz <marurh20@gmail.com>
2025-08-20 22:28:08 -0400

    Pre-style text

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import Index from "./app/index";
+import { CarroisGothicSC_400Regular, useFonts as useCarroisFonts } from '@expo-google-fonts/carrois-gothic-sc';
+import { Poppins_400Regular, Poppins_500Medium, useFonts as usePoppinsFonts } from '@expo-google-fonts/poppins';
+import { Syncopate_700Bold, useFonts as useSyncopateFonts } from '@expo-google-fonts/syncopate';
 
 export default function App() {
+
+  const [ carroisLoaded ] = useCarroisFonts({
+    CarroisGothicSC_400Regular
+  });
+  const [ poppinsLoaded ] = usePoppinsFonts({
+    Poppins_400Regular, Poppins_500Medium
+  });
+  const [ syncopateLoaded ] = useSyncopateFonts({
+    Syncopate_700Bold
+  });
+
+  if (!carroisLoaded || !poppinsLoaded || !syncopateLoaded) {
+    return null;
+  }
+
   return (
-    <Index />
+    <>
+      <Index />
+    </>
   );
 }
---
+++ components/StyledText.tsx
@@ -0,0 +1,51 @@
+import { Text, StyleSheet, type TextProps } from "react-native";
+
+export default function StyledText({ style, type='bodyCopy', ...rest }: TextProps & {
+  type?: 'pageHeader' | 'sectionHeader' | 'subheader' | 'bodyCopy' | 'labelsAndButtons';
+}) {
+    return (
+        <Text
+            style={[
+                type === 'pageHeader' ? styles.pageHeader : undefined,
+                type === 'sectionHeader' ? styles.sectionHeader : undefined,
+                type === 'subheader' ? styles.subheader : undefined,
+                type === 'bodyCopy' ? styles.bodyCopy : undefined,
+                type === 'labelsAndButtons' ? styles.labelsAndButtons : undefined,
+                style,
+            ]}
+            {...rest}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    pageHeader: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontFamily: "Syncopate_700Bold",
+    },
+
+    sectionHeader: {
+        fontSize: 16,
+        lineHeight: 20,
+        fontFamily: "CarroisGothicSC_400Regular",
+    },
+
+    subheader: {
+        fontSize: 13,
+        lineHeight: 18,
+        fontFamily: "Poppins_500Medium",
+    },
+
+    bodyCopy: {
+        fontSize: 13,
+        lineHeight: 18,
+        fontFamily: "Poppins_400Regular",
+    },
+
+    labelsAndButtons: {
+        fontSize: 10,
+        lineHeight: 20,
+        fontFamily: "CarroisGothicSC_400Regular",
+    }
+});
