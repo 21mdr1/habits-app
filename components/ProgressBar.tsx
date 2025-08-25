66ba88fafba98ceaf87bde4172c3f70393b0b1e1
Maria Rodriguez Hertz <marurh20@gmail.com>
2025-08-25 14:41:42 -0400

    Make progress bar update with task completion

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,13 +1,33 @@
 import { View, StyleSheet } from "react-native";
+import { useContext, useCallback } from "react";
 import { StyledText } from './Styled/StyledComponents';
 import { background, primary, textPrimary } from "@/utils/consts";
+import { RitualContext } from "@/utils/context";
 
 export default function ProgressBar() {
+    const { data: rituals } = useContext(RitualContext);
+
+    const getPercentage = useCallback(() => {
+        let allTasks: ITask[] = []
+
+        for(let ritual of rituals) {
+            allTasks = allTasks.concat(
+                ritual.tasks.filter(task => task.version[ritual.version - 1])
+            )
+        }
+
+        const total = allTasks.length
+        const completed = allTasks.filter(task => task.completed).length
+
+        return total !== 0 ? (completed / total)*100 : 100;
+    }, [rituals]);
+
+
     return (
         <View style={styles.container}>
             <StyledText type="subheader">Today's Progress</StyledText>
             <View style={styles.barEmpty}>
-                <View style={styles.barFull} />
+                <View style={[styles.barFull, { width: `${getPercentage()}%` } ]} />
             </View>
         </View>
     );
